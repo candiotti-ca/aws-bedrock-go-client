f628d2b50d52902052d274730557e4138ce73382
Carla <Carla.candiotti@hotmail.fr>
2023-11-14 15:45:56 +0100

    genericize client

--- client.go
+++ client.go
@@ -1,69 +1,49 @@
 package awsbedrockgoclient
 
 import (
+	"aws-bedrock-go-client/models"
 	"context"
 	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
-	"github.com/google/uuid"
 )
 
-type Model string
-
-const ModelCohereCommand Model = "cohere.command-text-v14"
-
-type InvokeModelBody struct {
-	Prompt string `json:"prompt"`
-}
-
-type InvokeModelResponse struct {
-	ID          uuid.UUID `json:"id"`
-	Prompt      string    `json:"prompt"`
-	Generations []struct {
-		FinishReason string    `json:"finish_reason"`
-		ID           uuid.UUID `json:"id"`
-		Text         string    `json:"text"`
-	} `json:"generations"`
-}
-
-type Client struct {
+type Client[I, O any] struct {
 	bedrockClient *bedrockruntime.Client
 	model         string
 }
 
-func New(cfg aws.Config, model Model) Client {
+func New[I, O any](cfg aws.Config, model models.Model) Client[I, O] {
 	b := bedrockruntime.NewFromConfig(cfg)
-	return Client{
+	return Client[I, O]{
 		bedrockClient: b,
 		model:         string(model),
 	}
 }
 
-// Query allows to query Aws Bedrock service with given prompt.
-func (c Client) Query(prompt string) (string, error) {
-	body, err := json.Marshal(InvokeModelBody{Prompt: prompt})
+func (c Client[I, O]) Query(ctx context.Context, input I) (*O, error) {
+	body, err := json.Marshal(input)
 	if err != nil {
-		return "", err
+		return nil, fmt.Errorf("cannot marshal input: %w", err)
 	}
 
-	output, err := c.bedrockClient.InvokeModel(context.Background(), &bedrockruntime.InvokeModelInput{
+	output, err := c.bedrockClient.InvokeModel(ctx, &bedrockruntime.InvokeModelInput{
 		ModelId:     &c.model,
-		Body:        []byte(body),
+		Body:        body,
 		Accept:      aws.String("*/*"),
 		ContentType: aws.String("application/json"),
 	})
-
 	if err != nil {
-		return "", fmt.Errorf("invoke model request has failed: %w", err)
+		return nil, fmt.Errorf("invoke model request has failed: %w", err)
 	}
 
-	response := InvokeModelResponse{}
-	err = json.Unmarshal(output.Body, &response)
+	var response *O
+	err = json.Unmarshal(output.Body, response)
 	if err != nil {
-		return "", err
+		return nil, fmt.Errorf("cannot unmarshal response: %w", err)
 	}
 
-	return response.Generations[0].Text, nil
+	return response, nil
 }
